a20266cb97ea821f60f453d6035e3436e628d641
Tia Petts <tpetts@madisoncollege.edu>
2021-12-20 20:00:00 -0600

    Create mock-heroes.ts
    Adding in some heroes and some not-technically-heroes, but good characters.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './heroes/heroes.component';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Superman' }
+    , { id: 2, name: 'Batman' }
+    , { id: 3, name: 'Cat Woman' }
+    , { id: 4, name: 'Harley Quinn' }
+    , { id: 5, name: 'Poison Ivy' }
+    , { id: 6, name: 'Flash' }
+    , { id: 7, name: 'Spiderman' }
+    , { id: 8, name: 'Deadpool' }
+    , { id: 9, name: 'Iron Man' }
+    , { id: 10, name: 'Ant Man' }
+    , { id: 11, name: 'Thor' }
+    , { id: 12, name: 'The Winter Soldier' }
+    , { id: 13, name: 'Loki, God of Mischief' }
+    , { id: 14, name: 'Captain America' }
+    , { id: 15, name: 'Aquaman' }
+    , { id: 16, name: 'The Incredible Hulk' }
+    , { id: 17, name: 'Black Widow' }
+    , { id: 18, name: 'Captain Jack Sparrow' }
+    , { id: 19, name: 'Robin' }
+    , { id: 20, name: 'Super Girl' }
+]
